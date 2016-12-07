55d9b9c855646fe0f18c2e60f6abf54690915bb7
slopezuag <serafinlopez@uagateway.org>
2016-12-07 21:11:28 +0000

    added rows and such

--- script.js
+++ script.js
@@ -1,1 +1,88 @@
 //Write your function calls below this line
+setColor(0, 0, "black");
+setColor(0, 1, "black");
+setColor(0, 2, "black");
+setColor(0, 3, "black");
+setColor(0, 4, "black");
+setColor(0, 5, "black");
+setColor(0, 6, "black");
+setColor(0, 7, "black");
+setColor(0, 8, "black");
+setColor(0, 9, "black");
+
+setColor(1, 0, "black");
+setColor(1, 1, "black");
+setColor(1, 2, "black");
+setColor(1, 3, "black");
+setColor(1, 4, "black");
+setColor(1, 5, "black");
+setColor(1, 6, "black");
+setColor(1, 7, "black");
+setColor(1, 8, "black");
+setColor(1, 9, "black");
+
+setColor(2, 0, "black");
+setColor(2, 1, "black");
+setColor(2, 2, "black");
+setColor(2, 3, "black");
+setColor(2, 4, "black");
+setColor(2, 5, "black");
+setColor(2, 6, "black");
+setColor(2, 7, "black");
+setColor(2, 8, "black");
+setColor(2, 9, "black");
+
+setColor(2, 0, "red");
+setColor(2, 1, "red");
+setColor(2, 2, "red");
+setColor(2, 3, "red");
+setColor(2, 4, "red");
+setColor(2, 5, "red");
+setColor(2, 6, "red");
+setColor(2, 7, "red");
+setColor(2, 8, "red");
+setColor(2, 9, "red");
+
+setColor(2, 0, "red");
+setColor(2, 1, "red");
+setColor(2, 2, "red");
+setColor(2, 3, "red");
+setColor(2, 4, "red");
+setColor(2, 5, "red");
+setColor(2, 6, "red");
+setColor(2, 7, "red");
+setColor(2, 8, "red");
+setColor(2, 9, "black");
+
+setColor(3, 0, "yellow");
+setColor(3, 1, "yellow");
+setColor(3, 2, "yellow");
+setColor(3, 3, "yellow");
+setColor(3, 4, "yellow");
+setColor(3, 5, "yellow");
+setColor(3, 6, "yellow");
+setColor(3, 7, "yellow");
+setColor(3, 8, "yellow");
+setColor(3, 9, "yellow");
+
+setColor(4, 0, "yellow");
+setColor(4, 1, "yellow");
+setColor(4, 2, "yellow");
+setColor(4, 3, "yellow");
+setColor(4, 4, "yellow");
+setColor(4, 5, "yellow");
+setColor(4, 6, "yellow");
+setColor(4, 7, "yellow");
+setColor(4, 8, "yellow");
+setColor(4, 9, "yellow");
+
+setColor(5, 0, "yellow");
+setColor(5, 1, "yellow");
+setColor(5, 2, "yellow");
+setColor(5, 3, "yellow");
+setColor(5, 4, "yellow");
+setColor(5, 5, "yellow");
+setColor(5, 6, "yellow");
+setColor(5, 7, "yellow");
+setColor(5, 8, "yellow");
+setColor(5, 9, "yellow");
